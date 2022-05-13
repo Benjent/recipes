8eb9981f59681785d438b1a91121455e4632da82
Benjent <Benjent@hotmail.fr>
2022-05-13 18:45:41 +0200

    U^dating deploy

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,21 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd dist
+# cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
+# git init
+# git checkout -b main
+git add dist -f
+git commit -m 'deploy dist'
+git subtree push --prefix dist origin gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Benjent/recipes.git main:gh-pages
+# git push -f git@github.com:Benjent/recipes.git main:gh-pages
 
 cd -
