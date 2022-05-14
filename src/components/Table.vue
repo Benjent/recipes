76868535a366193d03fbae7912cf67b6fb51d61c
Benjent <Benjent@hotmail.fr>
2022-05-14 11:31:48 +0200

    Remove this calls in templates

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -8,7 +8,7 @@ import sorting from "../utils/sorting"
     <table class="table">
         <thead class="table__head">
             <td :colspan="edibleColSpan" class="table__head__cell"></td>
-            <th scope="col" v-for="month in months" :key="getMonthName(month)" :class="{ 'table__head__cell': true, 'table__head__cell--highlight': month === this.currentMonth }">{{getMonthName(month)}}</th>
+            <th scope="col" v-for="month in months" :key="getMonthName(month)" :class="{ 'table__head__cell': true, 'table__head__cell--highlight': month === currentMonth }">{{getMonthName(month)}}</th>
         </thead>
         <tbody class="table__body">
             <tr v-for="edible, index in currentMonthEdibles" :key="edible.name" :class="[ 'table__body__row', 'table__body__row--current', { 'table__body__row--bordered': index === currentMonthEdibles.length - 1 }]">
@@ -17,7 +17,7 @@ import sorting from "../utils/sorting"
                     <img v-if="edible.months.includes(month) && edible.image" :src="edible.image"/>
                 </td>
             </tr>
-            <tr v-for="edible in edibles.filter((f) => !f.months.includes(this.currentMonth))" :key="edible.name">
+            <tr v-for="edible in edibles.filter((f) => !f.months.includes(currentMonth))" :key="edible.name">
                 <th :colspan="edibleColSpan" scope="row" class="table__body__cell--head">{{edible.name}}</th>
                 <td v-for="month in months" :key="`${edible.name}_${getMonthName(month)}`" :style="{ 'background': edible.months.includes(month) && edible.color }">
                     <img v-if="edible.months.includes(month) && edible.image" :src="edible.image"/>
