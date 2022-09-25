e910f3c3a63efb2afa64ca09223647194f05b2e8
Benjent <Benjent@hotmail.fr>
2022-09-25 09:58:16 +0200

    Add juice

--- src/data/juices.js
+++ src/data/juices.js
@@ -2,6 +2,7 @@ const juices = [
     { ingredients: ["Pomme", "Rhubarbe"] },
     { ingredients: ["Pomme", "Céleri"] },
     { ingredients: ["Pomme", "Orange", "Gingembre"] },
+    { ingredients: ["Abricot", "Orange"] },
     { ingredients: ["Carotte", "Citron", "Gingembre"] },
     { ingredients: ["Pastèque", "Citron", "Pomme", "Menthe"] },
     { ingredients: ["Brocoli", "Radis", "Tomate"] },
